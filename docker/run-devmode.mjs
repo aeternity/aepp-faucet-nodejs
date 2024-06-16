28daf321fcf9a564b18c6b21faf4899c1b943d45
Denis Davidyuk <denis_davidyuk@hotmail.com>
2024-06-16 13:57:32 +1000

    chore: add configuration to run locally

---
+++ docker/run-devmode.mjs
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import { execSync } from 'child_process';
+
+// TODO: remove after solving https://github.com/aeternity/ae_mdw/issues/1758
+try {
+  execSync(
+    'docker compose exec middleware ./bin/ae_mdw rpc ":aeplugin_dev_mode_app.start_unlink()"',
+    { stdio : 'pipe' },
+  );
+} catch (error) {
+  if (!error.message.includes('{:error, {:already_started')) throw error;
+}
